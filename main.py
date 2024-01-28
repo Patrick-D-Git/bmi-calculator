0bc2f93d1333540fa9c3a4b6223be77783a71cf7
Patrick D <patrick.montejo@gmail.com>
2024-01-27 22:09:36 -0500

    added bmi category result

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 # This is a simple BMI calculator
 
-weight = float(input("provide weight: "))
-height = float(input("provide height: "))
+weight = int(input("Please provide your weight: "))
+height = float(input("Please provide your height: "))
 
 bmi = int(weight/(height * height))
 
-print(bmi)
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
